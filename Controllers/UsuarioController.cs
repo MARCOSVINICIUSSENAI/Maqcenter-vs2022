bffb7c1b946f7b48fca59cf9057f6462cb7407e5
MARCOS VINICIUS <patriciniodasiLva@GMAIL.COM>
2024-07-24 17:04:39 -0300

    aula24/07

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+using MaqCenter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MaqCenter.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
